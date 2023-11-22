639a34eee5352cf49dccb73f1fe5edebb66ca076
N/A <gregory.weiner@gmail.com>
2023-11-22 01:50:43 -0600

    negate interpenetration of bodies by separation distance, distributed inversely proportional to their respective masses

--- code/Physics/Contact.cpp
+++ code/Physics/Contact.cpp
@@ -11,4 +11,20 @@ ResolveContact
 void ResolveContact( contact_t & contact ) {
 	contact.bodyA->m_linearVelocity.Zero();
 	contact.bodyB->m_linearVelocity.Zero();
+
+	// separate the colliding bodies such that their shared center of mass doesnt change
+	// collective center of mass is basically a barycentric avg of their positions,
+	// each weighted by their respective weights
+	const float massA		= 1.f / contact.bodyA->m_invMass;
+	const float massB		= 1.f / contact.bodyB->m_invMass;
+	const Vec3 centerOfMass = ( contact.bodyA->GetCenterOfMassModelSpace() * massA +
+								contact.bodyB->GetCenterOfMassModelSpace() * massB )
+							  / massA + massB;
+
+	const Vec3 separationDist =  contact.ptOnB_WorldSpace - contact.ptOnA_WorldSpace;
+	const float totalInvMass  =  contact.bodyA->m_invMass + contact.bodyB->m_invMass;
+	const float adjustPercA   =  contact.bodyA->m_invMass / totalInvMass;
+	const float adjustPercB   =  contact.bodyB->m_invMass / totalInvMass;
+	contact.bodyA->m_position += separationDist * adjustPercA;
+	contact.bodyB->m_position += separationDist * adjustPercB;
 }
