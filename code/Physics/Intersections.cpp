a592ac0d42ca69f767757fb91a49421308fb837e
N/A <gregory.weiner@gmail.com>
2023-11-26 19:26:46 -0600

    add raytrace function

--- code/Physics/Intersections.cpp
+++ code/Physics/Intersections.cpp
@@ -13,6 +13,38 @@ static constexpr Shape::shapeType_t sphereType = Shape::shapeType_t::SHAPE_SPHER
 
 /*
 ====================================================
+Intersect - Ray Sphere
+====================================================
+*/
+bool RaySphere( const Vec3 & rayStart, const Vec3 & rayDir, const Vec3 & sphereCenter, const float sphereRadii, float & t1, float & t2 ) {
+	const Vec3 pathToSphere		  = sphereCenter - rayStart;
+
+	const float rayDirMagSq		  = rayDir.Dot( rayDir );				// a
+	const float rayParallelToPath = pathToSphere.Dot( rayDir );			// b
+	// after accounting for the sum of both radii, 
+	// we see the distance for that outermost edge, from our raycast
+	// starting point. we need to solve for the scalar ( time, traversal ),
+	// that will get us from start point to that distance
+	const float distToSphereSurfaceSq = 
+		pathToSphere.Dot( pathToSphere ) - sphereRadii * sphereRadii;	// c
+
+	const float discriminant = rayParallelToPath * rayParallelToPath - rayDirMagSq * distToSphereSurfaceSq;
+	const float invA		 = 1.f / rayDirMagSq;
+
+	if ( discriminant < 0 ) {
+		// no solution exists
+		return false;
+	}
+
+	const float sqrtDiscriminant = sqrtf( discriminant );
+	t1 = invA * ( rayParallelToPath - sqrtDiscriminant );
+	t2 = invA * ( rayParallelToPath + sqrtDiscriminant );
+
+	return true;
+}
+
+/*
+====================================================
 Intersect - sphere to sphere only
 ====================================================
 */
