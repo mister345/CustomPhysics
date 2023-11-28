043ba7a3bb18e9e94ad5fbc52b4617bb57ace2a7
N/A <gregory.weiner@gmail.com>
2023-11-27 20:37:44 -0600

    setup scene to test time of impact

--- code/Scene.cpp
+++ code/Scene.cpp
@@ -51,32 +51,33 @@ Scene::Initialize
 */
 void Scene::Initialize() {
 	Body body;
-	body.m_position = Vec3( 0, 0, 15 );
+
+	body.m_position = Vec3( -3, 0, 3 );
 	body.m_orientation = Quat( 0, 0, 0, 1 );
-	body.m_linearVelocity = Vec3( 5, 0, 0 );
+	body.m_linearVelocity = Vec3( 1000, 0, 0 );
 	body.m_invMass = 1 / 1.f;
-	body.m_elasticity = 0.3f;
-	body.m_friction = 0.7f;
-	body.m_shape = new ShapeSphere( 5.0f );
+	body.m_elasticity = 0.f;
+	body.m_friction = 0.5f;
+	body.m_shape = new ShapeSphere( 0.5f );
 	m_bodies.push_back( body );
 
-	body.m_position = Vec3( 0, 15, 15 );
+	body.m_position = Vec3( 0, 0, 3 );
 	body.m_orientation = Quat( 0, 0, 0, 1 );
-	body.m_linearVelocity = Vec3( 0, 5, 0 );
-	body.m_invMass = 1 / 1000.f;
-	body.m_elasticity = 0.5f;
-	body.m_friction = 0.7f;
-	body.m_shape = new ShapeSphere( 5.0f );
+	body.m_linearVelocity = Vec3( 0, 0, 0 );
+	body.m_invMass = 0.f;
+	body.m_elasticity = 0.f;
+	body.m_friction = 0.5f;
+	body.m_shape = new ShapeSphere( 0.5f );
 	m_bodies.push_back( body );
 
 	// "ground" sphere unaffected by gravity
-	body.m_position = Vec3( 0, 0, -151 );
+	body.m_position = Vec3( 0, 0, -1000 );
 	body.m_orientation = Quat( 0, 0, 0, 1 );
 	body.m_linearVelocity = Vec3( 0, 0, 0 );
 	body.m_invMass = 0.f;
 	body.m_elasticity = 1.f;
-	body.m_friction = 0.5f;
-	body.m_shape = new ShapeSphere( 150.0f );
+	body.m_friction = 0.f;
+	body.m_shape = new ShapeSphere( 1000.0f );
 	m_bodies.push_back( body );
 }
 
