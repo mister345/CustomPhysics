d772b7ac39ff9319e709a8a801b2db11cb80baf4
N/A <gregory.weiner@gmail.com>
2023-11-27 23:08:34 -0600

    complete broadphase algorithm

--- code/Physics/Broadphase.cpp
+++ code/Physics/Broadphase.cpp
@@ -8,6 +8,7 @@
 Sweep and Prune algorithm
 ====================================================
 */
+namespace {
 struct pseudoBody_t {
 	int id;
 	float value;
@@ -23,8 +24,9 @@ int CompareSAP( const void * a, const void * b ) {
 	}
 	return 1;
 }
+}
 
-void SortBodiesBounds( const Body * bodies, const int num, pseudoBody_t * sortedArray, const float dt_sec ) {
+void SortBodiesBounds( const Body * bodies, const int num, pseudoBody_t * outSortedArray, const float dt_sec ) {
 	Vec3 axis = Vec3( 1, 1, 1 );
 	axis.Normalize();
 
@@ -42,16 +44,51 @@ void SortBodiesBounds( const Body * bodies, const int num, pseudoBody_t * sorted
 		bounds.Expand( bounds.maxs + Vec3(  1,  1,  1 ) * epsilon );
 
 		// separate each bounds into individual values, projected onto sweep axis so they can be sorted
-		// mins and maxes can later be tied together bc they share the same ids
-		sortedArray[ i * 2 + 0 ].id = i;
-		sortedArray[ i * 2 + 0 ].value = axis.Dot( bounds.mins );
-		sortedArray[ i * 2 + 0 ].isMin = true;
+		// mins and maxes can later be tied together bc they share the same ids= 0; 
+		outSortedArray[ i * 2 + 0 ].id = i;
+		outSortedArray[ i * 2 + 0 ].value = axis.Dot( bounds.mins );
+		outSortedArray[ i * 2 + 0 ].isMin = true;
 
-		sortedArray[ i * 2 + 1 ].id = i;
-		sortedArray[ i * 2 + 1 ].value = axis.Dot( bounds.maxs );
-		sortedArray[ i * 2 + 1 ].isMin = false;
+		outSortedArray[ i * 2 + 1 ].id = i;
+		outSortedArray[ i * 2 + 1 ].value = axis.Dot( bounds.maxs );
+		outSortedArray[ i * 2 + 1 ].isMin = false;
 	}
-	qsort( sortedArray, num * 2, sizeof( pseudoBody_t ), CompareSAP );
+	qsort( outSortedArray, num * 2, sizeof( pseudoBody_t ), CompareSAP );
+}
+
+void BuildPairs( std::vector< collisionPair_t > & collisionPairs, const pseudoBody_t * sortedBodies, const int num ) {
+	collisionPairs.clear();
+
+	for ( int i = 0; i < num * 2; i++ ) {
+		const pseudoBody_t & a = sortedBodies[ i ];
+		if ( !a.isMin ) {
+			continue;
+		}
+
+		collisionPair_t pair;
+		pair.a = a.id;
+
+		for ( int j = i + 1; j < num * 2; j++ ) {
+			const pseudoBody_t & b = sortedBodies[ j ];
+			if ( b.id == a.id ) {
+				break;
+			}
+
+			if ( !b.isMin ) {
+				continue;
+			}
+
+			pair.b = b.id;
+			collisionPairs.push_back( pair );
+		}
+	}
+}
+
+void SweepAndPrune1D( const Body * bodies, const int num, std::vector< collisionPair_t > & finalPairs, const float dt_sec ) {
+	pseudoBody_t * sortedBodies = reinterpret_cast< pseudoBody_t * >( alloca( sizeof( pseudoBody_t ) * num * 2 ) );
+
+	SortBodiesBounds( bodies, num, sortedBodies, dt_sec );
+	BuildPairs( finalPairs, sortedBodies, num );
 }
 
 /*
@@ -60,5 +97,7 @@ BroadPhase
 ====================================================
 */
 void BroadPhase( const Body * bodies, const int num, std::vector< collisionPair_t > & finalPairs, const float dt_sec ) {
-	// TODO: Add Code
+	finalPairs.clear();
+
+	SweepAndPrune1D( bodies, num, finalPairs, dt_sec );
 }
