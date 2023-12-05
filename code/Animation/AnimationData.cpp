3764211b09bde3c54284c0911a51ce9e51e690a6
N/A <gregory.weiner@gmail.com>
2023-12-05 12:21:01 -0800

    former fbx model rig was completely broken, replace also print more info about the loaded rig conventions

--- code/Animation/AnimationData.cpp
+++ code/Animation/AnimationData.cpp
@@ -125,6 +125,99 @@ void SkinnedData::Set(
 	mAnimations.insert( animations.begin(), animations.end() );
 }
 
+namespace FbxUtil{
+bool IsBone( fbxsdk::FbxNode * node ) {
+	return node->GetNodeAttribute() &&
+		   node->GetNodeAttribute()->GetAttributeType() == fbxsdk::FbxNodeAttribute::EType::eSkeleton;
+}
+
+void PrintBoneTransform( fbxsdk::FbxNode * pNode ) {
+	fbxsdk::FbxAMatrix localTransform = pNode->EvaluateLocalTransform();
+	fbxsdk::FbxVector4 translation = localTransform.GetT();
+	fbxsdk::FbxVector4 rotation = localTransform.GetR();
+	fbxsdk::FbxVector4 scaling = localTransform.GetS();
+
+	printf( "    Bone Transform:\n" );
+	printf( "        Translation: %f, %f, %f\n", translation[ 0 ], translation[ 1 ], translation[ 2 ] );
+	printf( "        Rotation: %f, %f, %f\n", rotation[ 0 ], rotation[ 1 ], rotation[ 2 ] );
+	printf( "        Scaling: %f, %f, %f\n", scaling[ 0 ], scaling[ 1 ], scaling[ 2 ] );
+}
+
+void PrintNode( fbxsdk::FbxNode * pNode ) {
+	if ( pNode != nullptr ) {
+		printf( "Node Name: %s\n", pNode->GetName() );
+		// Print rotation order
+		fbxsdk::FbxEuler::EOrder rotOrder;
+		pNode->GetRotationOrder( fbxsdk::FbxNode::eSourcePivot, rotOrder );
+		std::string rotOrderStr;
+		switch ( rotOrder ) {
+			case fbxsdk::FbxEuler::eEulerXYZ: rotOrderStr = "XYZ"; break;
+			case fbxsdk::FbxEuler::eEulerXZY: rotOrderStr = "XZY"; break;
+			case fbxsdk::FbxEuler::eEulerYZX: rotOrderStr = "YZX"; break;
+			case fbxsdk::FbxEuler::eEulerYXZ: rotOrderStr = "YXZ"; break;
+			case fbxsdk::FbxEuler::eEulerZXY: rotOrderStr = "ZXY"; break;
+			case fbxsdk::FbxEuler::eEulerZYX: rotOrderStr = "ZYX"; break;
+			case fbxsdk::FbxEuler::eSphericXYZ: rotOrderStr = "Spherical XYZ"; break;
+			default: rotOrderStr = "Unknown"; break;
+		}
+		printf( "    Rotation Order: %s\n", rotOrderStr.c_str() );
+
+		for ( int i = 0; i < pNode->GetNodeAttributeCount(); i++ ) {
+			fbxsdk::FbxNodeAttribute * pAttribute = pNode->GetNodeAttributeByIndex( i );
+			if ( pAttribute != nullptr ) {
+				fbxsdk::FbxString typeName = pAttribute->GetTypeName();
+				fbxsdk::FbxString attrName = pAttribute->GetName();
+				printf( "    Attribute Type: %s\n", typeName.Buffer() );
+				printf( "    Attribute Name: %s\n", attrName.Buffer() );
+				if ( IsBone( pNode ) ) {
+					PrintBoneTransform( pNode );
+				}
+			}
+		}
+		for ( int j = 0; j < pNode->GetChildCount(); j++ ) {
+			PrintNode( pNode->GetChild( j ) );
+		}
+	}
+}
+
+void PrintScene( fbxsdk::FbxScene * pScene ) {
+	printf( "Scene Name: %s\n--------------------------------------\n", pScene->GetName() );
+
+	// Get and print coordinate system information
+	fbxsdk::FbxAxisSystem axisSystem = pScene->GetGlobalSettings().GetAxisSystem();
+	std::string coordSystem = axisSystem.GetCoorSystem() == fbxsdk::FbxAxisSystem::eRightHanded ? "Right-Handed" : "Left-Handed";
+	printf( "Coordinate System: %s\n", coordSystem.c_str() );
+
+	// Determine and print the Up, Front and Right Axis
+	int upAxisSign, frontAxisSign;
+	fbxsdk::FbxAxisSystem::EUpVector upAxis = axisSystem.GetUpVector( upAxisSign );
+	fbxsdk::FbxAxisSystem::EFrontVector frontAxis = axisSystem.GetFrontVector( frontAxisSign );
+
+	// Determine Right Axis based on the right-handed or left-handed coordinate system
+	std::string rightAxis = coordSystem == "Right-Handed" ? "+X" : "-X";
+
+	// Determine Up Axis
+	std::string upAxisString = ( upAxis == fbxsdk::FbxAxisSystem::eXAxis ? "X" : ( upAxis == fbxsdk::FbxAxisSystem::eYAxis ? "Y" : "Z" ) );
+	upAxisString = ( upAxisSign > 0 ? "+" : "-" ) + upAxisString;
+
+	// Determine Front Axis (assuming Z is the default front axis)
+	std::string frontAxisString = ( frontAxis == fbxsdk::FbxAxisSystem::eParityOdd ? "-Z" : "+Z" );
+
+	// Print axes in X, Y, Z order
+	printf( "Right Axis: %s\n", rightAxis.c_str() );
+	printf( "Up    Axis: %s\n", upAxisString.c_str() );
+	printf( "Fwd   Axis: %s\n", frontAxisString.c_str() );
+
+	// print nodes
+	fbxsdk::FbxNode * pRootNode = pScene->GetRootNode();
+	if ( pRootNode != nullptr ) {
+		for ( int i = 0; i < pRootNode->GetChildCount(); i++ ) {
+			PrintNode( pRootNode->GetChild( i ) );
+		}
+	}
+}
+} // namepsace fbx util
+
 void SkinnedData::Set( fbxsdk::FbxScene * scene ) {
 
 	std::vector< int > bones;
@@ -132,41 +225,8 @@ void SkinnedData::Set( fbxsdk::FbxScene * scene ) {
 	AnimationClip clip;
 	std::map< std::string, AnimationClip > anims = { { "TODO", clip } };
 
-	////////////////////////////////////////////////
-	// Extract all the above data from the scene
-	struct local_t {
-		static void PrintNode( fbxsdk::FbxNode * pNode ) {
-			if ( pNode != nullptr ) {
-				printf( "Node Name: %s\n", pNode->GetName() );
-				for ( int i = 0; i < pNode->GetNodeAttributeCount(); i++ ) {
-					fbxsdk::FbxNodeAttribute * pAttribute = pNode->GetNodeAttributeByIndex( i );
-					if ( pAttribute != nullptr ) {
-						fbxsdk::FbxString typeName = pAttribute->GetTypeName();
-						fbxsdk::FbxString attrName = pAttribute->GetName();
-						printf( "    Attribute Type: %s\n", typeName.Buffer() );
-						printf( "    Attribute Name: %s\n", attrName.Buffer() );
-					}
-				}
-				for ( int j = 0; j < pNode->GetChildCount(); j++ ) {
-					PrintNode( pNode->GetChild( j ) );
-				}
-			}
-		}
-		static void PrintScene( fbxsdk::FbxScene * pScene ) {
-			printf( "Scene Name: %s\n--------------------------------------\n", pScene->GetName() );
-			
-			fbxsdk::FbxNode * pRootNode = pScene->GetRootNode();
-			if ( pRootNode != nullptr ) {
-				for ( int i = 0; i < pRootNode->GetChildCount(); i++ ) {
-					PrintNode( pRootNode->GetChild( i ) );
-				}
-			}
-		}
-	};
+	FbxUtil::PrintScene( scene );
 
-	local_t::PrintScene( scene );
-
-	////////////////////////////////////////////////
 	Set( bones, refPose, anims );
 }
 
